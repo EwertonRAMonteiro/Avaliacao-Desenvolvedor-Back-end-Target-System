48456b7e3c5295f3d777142307d8a82a74c8064c
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2023-01-31 22:18:18 -0300

    exercico cinco finalizado

---
+++ src/main/java/com/targetsystem/desafio/atvCinco/InverterController.java
@@ -0,0 +1,74 @@
+package com.targetsystem.desafio.atvCinco;
+
+public class InverterController {
+
+    public String one(){
+        String inveter = "notrewe";
+        String invertido = "";
+
+        for (int i = inveter.length() -1; i >= 0; --i) {
+            invertido += Character.toString(inveter.charAt(i));
+        }
+        return invertido;
+    }
+
+    public String two(){
+        String inveter = "ibur";
+        String invertido = "";
+
+        for (int i = inveter.length() -1; i >= 0; --i) {
+            invertido += Character.toString(inveter.charAt(i));
+        }
+        return invertido;
+    }
+
+    public String three(){
+        String inveter = "rodamargorp";
+        String invertido = "";
+
+        for (int i = inveter.length() -1; i >= 0; --i) {
+            invertido += Character.toString(inveter.charAt(i));
+        }
+        return invertido;
+    }
+
+    public String forr(){
+        String inveter = "dnekcab";
+        String invertido = "";
+
+        for (int i = inveter.length() -1; i >= 0; --i) {
+            invertido += Character.toString(inveter.charAt(i));
+        }
+        return invertido;
+    }
+
+    public String five(){
+        String inveter = "roinuj";
+        String invertido = "";
+
+        for (int i = inveter.length() -1; i >= 0; --i) {
+            invertido += Character.toString(inveter.charAt(i));
+        }
+        return invertido;
+    }
+
+    public String six(){
+        String inveter = "tegraT";
+        String invertido = "";
+
+        for (int i = inveter.length() -1; i >= 0; --i) {
+            invertido += Character.toString(inveter.charAt(i));
+        }
+        return invertido;
+    }
+
+    public String seven(){
+        String inveter = "metsyS";
+        String invertido = "";
+
+        for (int i = inveter.length() -1; i >= 0; --i) {
+            invertido += Character.toString(inveter.charAt(i));
+        }
+        return invertido;
+    }
+}
---
+++ src/main/java/com/targetsystem/desafio/atvCinco/InverterString.java
@@ -0,0 +1,31 @@
+package com.targetsystem.desafio.atvCinco;
+
+public class InverterString {
+
+    public static void main(String[] args) {
+
+        InverterController inverterController = new InverterController();
+
+            System.out.println(
+                    inverterController.one() + " " +
+                    inverterController.two() + " " +
+                    inverterController.three() + " " +
+                    inverterController.forr() + " " +
+                    inverterController.five() + " " +
+                    inverterController.six() + " " +
+                    inverterController.seven()
+            );
+        System.out.println(
+                inverterController.one() + " " +
+                        inverterController.two() + " " +
+                        inverterController.three() + " " +
+                        inverterController.forr() + " " +
+                        inverterController.five() + " " +
+                        inverterController.six() + " " +
+                        inverterController.seven()
+        );
+        }
+
+    }
+
+
